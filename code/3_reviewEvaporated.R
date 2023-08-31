811f2de88a08b72519f6e1d59526c862957b0fef
Gabriel Bowen <gabe.bowen@utah.edu>
2023-08-31 15:21:39 -0600

    Update mixing and evap

--- code/3_reviewEvaporated.R
+++ code/3_reviewEvaporated.R
@@ -3,27 +3,41 @@ library(rgbif)
 # Prep ####
 ## Read data
 p = read.csv("data/plants.csv")
+s = read.csv("data/soils.csv")
 
 # Review Dex ####
 ## Calculate and distribution
 p$Dex = p$d2H - p$d18O * 8
 plot(density(p$Dex))
 
+## Minimum Dex soil values per bout
+bouts = unique(s$Bout)
+s.dex.min = numeric(length(bouts))
+for(i in seq_along(bouts)){
+  s.bout = s[s$Bout == bouts[i],]
+  s.dex.min[i] = min(s.bout$d2H - 8 * s.bout$d18O)
+}
+
+## Compare plant values with minimum soil values
+p$Dex.off = p$Dex - s.dex.min[match(p$Bout, bouts)]
+plot(density(p$Dex.off))
+abline(v = -30, lty = 3)
+
 ## Low values
-p.low = p[p$Dex < -40,]
+p.low = p[p$Dex.off < -30,]
 
 ## Species and counts
-s.low = data.frame(table(p.low$Species))
+sp.low = data.frame(table(p.low$Species))
 
 ## Get common names from GBIF
-s.low$Vernacular = rep("")
-for(i in seq_along(s.low$Var1)){
-  gbif = name_backbone(s.low$Var1[i])
+sp.low$Vernacular = rep("")
+for(i in seq_along(sp.low$Var1)){
+  gbif = name_backbone(sp.low$Var1[i])
   use = as.data.frame(name_usage(gbif$usageKey)$data)
   if(length(use$vernacularName > 0)){
-    s.low$Vernacular[i] = use$vernacularName
+    sp.low$Vernacular[i] = use$vernacularName
   }
 }
 
 ## Review - mostly shrubs and grasses
-View(s.low)
+View(sp.low)
